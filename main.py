c596e3fdab1640ca373825ba98ffd682c3d8ec51
Luis Batalha <lfrb25@gmail.com>
2016-05-29 16:00:29 -0700

    handle missing paste data

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
 import pygments
-
 from flask import Flask, render_template, url_for, request
-
 
 app = Flask(__name__)
 
@@ -11,15 +9,21 @@ lexer = 'txt'
 ttl = 60
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-	
+def newpaste():
+	paste = None
 	if request.method == 'POST':
+		if 'paste' in request.form and request.form['paste']:
+			paste = request.form['paste']
+		else:
+			return "pls, actually paste something k?\n"
 		if request.form['lexer']:
 			lexer = request.form['lexer']
 		if request.form['ttl']:
 			ttl = int(request.form['ttl'])
-
-	return render_template('index.html')
+		print(ttl)
+		print(lexer)
+		print(paste)
+	return render_template('newpaste.html')
 
 
 if __name__ == '__main__':
