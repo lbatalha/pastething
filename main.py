032dbcc461fa988a7cd67275438f9c2f946e7c63
Luis Batalha <lfrb25@gmail.com>
2016-06-04 15:26:28 -0700

    add config file fix ordered list

--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import random
-
 from base64 import urlsafe_b64encode
 
-#----------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 import pygments
 from pygments import highlight
@@ -16,25 +15,21 @@ from flask import Flask
 from flask import render_template, url_for, flash
 from flask import request, redirect, Response, abort
 
+import config
+
 app = Flask(__name__)
-app.secret_key = 'some_secret'
+app.secret_key = config.secret_key
 
 rng = random.SystemRandom()
 
-plain_useragents = ('curl','wget', None)
-lexer = 'txt'
-ttl = 60
-url_len = 1
-url_alph = tuple("23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz")
-base = len(url_alph)
 
 def base_encode(num):
 	if not num:
-		return url_alph[0]
+		return config.url_alph[0]
 	result = ''
 	while num:
-		num, rem = divmod(num, base)
-		result = result.join(url_alph[rem])
+		num, rem = divmod(num, config.base)
+		result = result.join(config.url_alph[rem])
 	return result
 
 def plain(text):
@@ -47,23 +42,25 @@ def paste_stats(text):
 	stats['lines'] = len(text.split('\n'))
 	stats['sloc'] = stats['lines'] - len(text.split('\n\n'))
 	stats['size'] = len(text.encode('utf-8'))
+	stats['lexer'] = 'python'
 	return stats
 
 
 @app.route('/', methods=['GET', 'POST'])
 def newpaste():
 	if request.method == 'POST':
-
-		paste = None
 		if 'paste' in request.form and request.form['paste']:
 			paste = request.form['paste']
 		else:
 			return "pls, actually paste something k?\n"
-		if request.form['lexer']:
+		if 'lexer' in request.form and request.form['lexer']:
 			lexer = request.form['lexer']
-		if request.form['ttl']:
+		else:
+			lexer = config.lexer
+		if 'ttl' in request.form and request.form['ttl']:
 			ttl = int(request.form['ttl'])
-		
+		else:
+			ttl = config.ttl
 		print(ttl)
 		print(lexer)
 		print(paste)
@@ -77,7 +74,7 @@ def newpaste():
 			#url_len += 1
 			##placeholder for collision check
 		print(url)
-		flash(urlsafe_b64encode(rng.getrandbits(48).to_bytes(6, 'little')))
+		flash(urlsafe_b64encode(rng.getrandbits(48).to_bytes(6, 'little')).decode('utf-8'))
 		return redirect(url_for('viewpaste'))
 	return render_template('newpaste.html')
 
@@ -90,11 +87,7 @@ def viewpaste():
 
 		stats = paste_stats(paste)
 
-		user_agent = request.user_agent.browser
-		for ua in plain_useragents:
-			if user_agent == ua:
-				return plain(paste)
-		if request.args.get('plain') is not None:
+		if request.args.get('r') is not None:
 				return plain(paste)
 
 		direction = 'ltr'
@@ -108,11 +101,11 @@ def viewpaste():
 		text = paste
 		try:
 			lexer = get_lexer_by_name('python')
-			formatter = HtmlFormatter(nowrap=False, linenos=False, cssclass='paste')
+			formatter = HtmlFormatter(nowrap=True, cssclass='paste')
 			paste = highlight(paste, lexer, formatter)
 		except pygments.util.ClassNotFound:
 			paste = text
-		
+
 		return render_template('viewpaste.html', stats=stats, paste=paste.split("\n"), direction=direction)
 
 if __name__ == '__main__':
