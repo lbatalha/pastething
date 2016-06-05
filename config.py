a9507aef79b3e2b41ed52466697324baa299effb
Luis Batalha <lfrb25@gmail.com>
2016-06-04 20:18:33 -0700

    finish js functions finish layout for paste page preliminary new paste page styling

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ ttl = 60
 lexer = 'txt'
 
 secret_key = 'some_secret'
-
+max_content_length = 10 * 1024 * 1024
 url_len = 1
 url_alph = tuple("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghkmnpqrstuvwxyz")
 base = len(url_alph)
