e79f69c7883c2e487508be21410072065f863819
Luis Batalha <lfrb25@gmail.com>
2016-06-05 15:00:37 -0700

    add prefered paste config

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 #new paste defaults
 defaults = {
 	'ttl': 1.0,
-	'lexer': 'txt',
+	'lexer': 'auto',
+	'burn': 0,
 	'paste': '',
-
 }
 
 #paste options
@@ -23,3 +23,39 @@ base = len(url_alph)
 #error messages
 empty_paste = "pls, actually paste something k?\n"
 invalid_ttl = "ttl must be between " + str(ttl_min) + " and " + str(ttl_max) +" hours\n"
+
+
+#form options
+ttl_options = {
+	'5 minutes': 0.0833,
+	'15 minutes': 0.25,
+	'30 minutes': 0.5,
+	'1 hour': 1,
+	'3 hours': 3,
+	'6 hours': 6,
+	'12 hours': 12,
+	'1 day': 24,
+	'3 days': 72,
+	'1 week': 168,
+	'2 weeks': 336,
+	'1 month': 730,
+}
+
+common_lexers = {
+	'Bash': 'bash',
+	'C': 'c',
+	'C++': 'cpp',
+	'C#': 'csharp',
+	'Python': 'python',
+	'Ruby': 'ruby',
+	'Go': 'go',
+	'Perl': 'perl',
+	'PHP': 'php',
+	'CSS': 'css',
+	'HTML': 'html',
+	'JavaScript': 'javascript',
+	'JSON': 'json',
+	'XML': 'xml',
+	'SQL': 'sql',
+}
+
