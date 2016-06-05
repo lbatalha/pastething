c8b817844db2e9ef876a113584a0562289bf2af0
Luis Batalha <lfrb25@gmail.com>
2016-06-05 13:44:45 -0700

    refactor post parameter validation

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
-ttl = 60
-lexer = 'txt'
+#new paste defaults
 
+#ttl = 60 #default paste TTL
+#lexer = 'txt' #default paste lexer
+defaults = {
+	'ttl': 60,
+	'lexer': 'txt',
+	'paste': '',
+}
+
+#app configuration
 secret_key = 'some_secret'
-max_content_length = 10 * 1024 * 1024
-url_len = 1
-url_alph = tuple("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghkmnpqrstuvwxyz")
+max_content_length = 10 * 1024 * 1024 #max form upload size
+
+#paste url
+url_len = 1 #minimum paste url length
+url_alph = tuple("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghkmnpqrstuvwxyz") #alphabet used to generate paste url
 base = len(url_alph)
