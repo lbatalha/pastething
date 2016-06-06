6eafea3f6b9772836639ba3d470676bfd02905bc
lbatalha <lfrb25@gmail.com>
2016-06-06 18:41:03 +0100

    implement appending paste to db

--- db.sql
+++ db.sql
@@ -1,16 +1,16 @@
 \connect postgres
 DROP DATABASE IF EXISTS pastebin;
-CREATE ROLE pastebin WITH PASSWORD '1234';
+CREATE ROLE pastebin WITH LOGIN PASSWORD '1234';
 CREATE DATABASE pastebin OWNER pastebin;
-GRANT USAGE ON DATABASE pastebin TO pastebin;
+GRANT ALL PRIVILEGES ON DATABASE pastebin TO pastebin;
 \connect pastebin
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO pastebin;
 CREATE TABLE pastes(
 	pasteid 	varchar(10) PRIMARY KEY NOT NULL,
-	token 		varchar(32) 			NOT NULL,
-	lexer 		varchar(512) 			NOT NULL,
-	expiration 	timestamp 				NOT NULL,
-	burn 		int 					NOT NULL,
-	paste 		text 					NOT NULL
+	token 		varchar(32)		NOT NULL,
+	lexer 		varchar(512) 		NOT NULL,
+	expiration 	timestamp 		NOT NULL,
+	burn 		int 			NOT NULL,
+	paste 		text 			NOT NULL
 );
 
