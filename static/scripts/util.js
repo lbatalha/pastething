1195c15d94c6600745ec2f56b554c91a25f3a127
Luis Batalha <lfrb25@gmail.com>
2016-06-11 08:06:39 -0700

    fix JS regression fix css linewrap issue

--- static/scripts/util.js
+++ static/scripts/util.js
@@ -4,33 +4,38 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	document.getElementById("dir").addEventListener("click", dir);
 	document.getElementById("indent").addEventListener("click", indent);
 });
-function wrap(evt){
-    var lines = document.getElementsByClassName("pastelines");
-    for(i in lines){
-        lines[i].style.whiteSpace = 
-            (window.getComputedStyle(lines[i]).getPropertyValue("white-space") == "pre") ?
-            "pre-wrap" :
-            "pre";
-    }
-    evt.preventDefault
+function wrap(evt) {
+	evt.preventDefault();
+	var lines = document.getElementsByClassName("pastelines");
+	for(var i = 0, il = lines.length; i < il; i++){
+		if(window.getComputedStyle(lines[i]).getPropertyValue("white-space") == "pre"){
+			lines[i].style.whiteSpace = "pre-wrap";
+		}else{
+			lines[i].style.whiteSpace = "pre";
+		}
+	}
 }
 function dir(evt) {
-	var dir = document.getElementById("wrapper");
-    dir.setAttribute(
-        "dir",
-        dir.getAttribute("dir").split('').reverse().join('')
-    );
 	evt.preventDefault();
+	var dir = document.getElementById("wrapper");
+	dir.setAttribute(
+		"dir",
+		dir.getAttribute("dir").split('').reverse().join('')
+	);
+
 }
 function indent(evt) {
-	var lines = document.getElementsByClassName("pastelines");
-	for(i in lines){
-        lines[i].style.tabSize = "2";
-		lines[i].style.tabSize =
-            (window.getComputedStyle(lines[i]).getPropertyValue("tab-size") == "2") ?
-            "4" :
-            "8";
-	}
 	evt.preventDefault();
+	var lines = document.getElementsByClassName("pastelines");
+	for(var i = 0, il = lines.length; i < il; i++){
+		if(window.getComputedStyle(lines[i]).getPropertyValue("tab-size") == "2"){
+			lines[i].style.tabSize = "4";
+		}else if(window.getComputedStyle(lines[i]).getPropertyValue("tab-size") == "4"){
+			lines[i].style.tabSize = "8";
+		}else{
+			lines[i].style.tabSize = "2";
+		}
+		
+	}
+	
 }
-
