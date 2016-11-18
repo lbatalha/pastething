957507848bacb402069ee72fb1d11c6ce9a8674f
lbatalha <lfrb25@gmail.com>
2016-11-18 14:31:54 +0000

    remove urlgen test

--- tests.py
+++ tests.py
@@ -5,10 +5,6 @@ import gc
 
 from random import getrandbits
 
-def test_urlgen():
-	for i in range(256):
-		assert main.base_encode(getrandbits(config.url_alph_bits)) in config.url_alph
-
 def test_plainresponse():
 	assert main.plain('test').headers['Content-Type'] == 'text/plain; charset=utf-8'
 
