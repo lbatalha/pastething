fbd9750aa0fbfb8b03c9844f8e9a6cb6db6a6832
lbatalha <lfrb25@gmail.com>
2016-11-23 21:32:53 +0000

    fix error handling

--- main.py
+++ main.py
@@ -264,11 +264,11 @@ def statspage():
 
 
 @app.errorhandler(404)
-def page_not_found():
+def page_not_found(e):
 	return render_template('404.html'), 404
 
 @app.errorhandler(500)
-def internal_server_error():
+def internal_server_error(e):
 	return render_template('500.html'), 500
 
 if __name__ == '__main__':
