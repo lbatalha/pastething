d29afc5a6d518f13058d7ee03541d65c2e185a70
lbatalha <lfrb25@gmail.com>
2016-11-28 17:42:09 +0000

    add several tests

--- tests.py
+++ tests.py
@@ -3,13 +3,33 @@ import config
 import stats
 import gc
 
-import requests
+import requests, subprocess
 from random import getrandbits
+from time import sleep
 
 url = "http://localhost:5000/"
 
 def test_plainresponse():
-	assert main.plain('test').headers['Content-Type'] == 'text/plain; charset=utf-8'
+	params = {'paste': 'test', 'raw': 'true'}
+	r = requests.post(url, data=params)
+	response = r.text.split(" | ")
+	r = requests.get(response[0])
+	assert r.headers['content-type'] == 'text/plain; charset=utf-8'
+	params['raw'] = ''
+	r = requests.post(url, data=params)
+	response = r.text.split(" | ")
+	r = requests.get(response[0])
+	assert r.headers['content-type'] == 'text/html; charset=utf-8'
+
+
+
+def test_pastedelete():
+	params = {'paste': 'test', 'burn': 1, 'ttl': 1.0}
+	r = requests.post(url, data=params)
+	assert r.status_code == 200
+	response = r.text.split(" | ")
+	r = requests.delete(response[0], data={'token': response[1][:-1]})
+	assert r.status_code == 200
 
 def test_postlimits():
 	#Missing paste body test
@@ -41,3 +61,32 @@ def test_postlimits():
 	r = requests.post(url, data=params)
 	assert r.status_code != 200
 
+
+def test_pasteburn():
+	"""test for paste burn auto-deletion"""
+	params = {'paste': 'test', 'burn': 2, 'ttl': 1.0}
+	r = requests.post(url, data=params)
+	assert r.status_code == 200
+	response = r.text.split(" | ")
+	r = requests.get(response[0])
+	assert r.status_code == 200
+	r = requests.get(response[0])
+	r = requests.get(response[0])
+	assert r.status_code == 404
+
+def test_pastettl():
+	"""test for paste auto-delete on expiration"""
+	ttl = 0.001 
+	seconds = 60*60*0.001
+	params = {'paste': 'test', 'ttl': ttl}
+	r = requests.post(url, data=params)
+	assert r.status_code == 200
+	response = r.text.split(" | ")
+	r = requests.get(response[0])
+	assert r.status_code == 200
+	sleep(seconds)
+	r = requests.get(response[0])
+	assert r.status_code == 404
+
+def test_garbagecollect():
+	assert subprocess.run(['python3', 'gc.py']).returncode == 0
